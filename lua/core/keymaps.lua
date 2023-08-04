02d9b6bf7871b2ced7965603897dce30a0cae3b8
aqiu <819110812@qq.com>
2023-08-04 22:32:11 +0800

    add nvim config

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,21 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+
+-- ---------- 视觉模式 ---------- ---
+
+-- ---------- 正常模式 ---------- ---
+-- 窗口
+keymap.set("n", "<leader>ll", "<C-w>v") -- 水平新增窗口 
+
+-- 取消高亮
+keymap.set("n", "<leader>nn", ":nohl<CR>")
+
+-- 切换buffer
+keymap.set("n", "<C-L>", ":bnext<CR>")
+keymap.set("n", "<C-H>", ":bprevious<CR>")
+
+-- ---------- 插件 ---------- ---
+-- nvim-tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
